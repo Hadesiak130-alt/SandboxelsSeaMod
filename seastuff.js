b0b80beb8c4e1e3db30968e2ef4bfc474d576627
Hadesiak130-alt <agnieszkafraczkowska89+github@gmail.com>
2025-08-06 18:57:04 +0200

    Update seastuff.js

--- seastuff.js
+++ seastuff.js
@@ -3,7 +3,6 @@ elements.sea_water = {
     color: "#406080",
     behavior: behaviors.LIQUID,
     category: "liquids",
-    state: "liquid",
     density: 1025, // slightly denser than water
     tempHigh: 100,
     stateHigh: ["steam", "sea_salt"],
@@ -18,7 +17,6 @@ elements.sea_salt = {
     color: "#dcdcdc",
     behavior: behaviors.POWDER,
     category: "powders",
-    state: "solid",
     density: 2160,
     reactions: {
         water: { elem1: null, elem2: "salt_water", chance: 0.8 } // dissolves in water
