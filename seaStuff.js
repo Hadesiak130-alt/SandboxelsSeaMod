19f9b4305608f9a7a339bd6ab12b2c1b6a985f8b
Hadesiak130-alt <agnieszkafraczkowska89+github@gmail.com>
2025-08-06 20:05:03 +0200

    Update seaStuff.js

--- seaStuff.js
+++ seaStuff.js
@@ -1,11 +1,17 @@
-function addSeaElements() {
-  if (typeof elements === "undefined") {
-    setTimeout(addSeaElements, 100); // wait until elements exists
-    return;
-  }
+// Sea Salt and Sea Water mod for Sandboxels
 
-  // Add your elements here
-  elements.sea_water = {
+elements.sea_salt = {
+    color: "#dcdcdc",
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state: "solid",
+    density: 2160,
+    reactions: {
+        water: { elem1: null, elem2: "salt_water", chance: 0.8 }
+    }
+};
+
+elements.sea_water = {
     color: "#406080",
     behavior: behaviors.LIQUID,
     category: "liquids",
@@ -14,20 +20,6 @@ function addSeaElements() {
     tempHigh: 100,
     stateHigh: ["steam", "sea_salt"],
     reactions: {
-      fire: { elem2: "steam", chance: 0.2 }
+        fire: { elem2: "steam", chance: 0.2 }
     }
-  };
-
-  elements.sea_salt = {
-    color: "#dcdcdc",
-    behavior: behaviors.POWDER,
-    category: "powders",
-    state: "solid",
-    density: 2160,
-    reactions: {
-      water: { elem1: null, elem2: "salt_water", chance: 0.8 }
-    }
-  };
-}
-
-addSeaElements();
+};
